e91e7ae7e23e38f6ea68f750f5f7ee4edd31aba5
Andrew Lefler <lefleraa@gmail.com>
2018-04-22 19:39:35 -0400

    simplify and streamline gulp build task to ensure full build on first compile

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,42 @@ var less   = require('gulp-less');
 var path   = require('path');
 var clean  = require('gulp-clean-css');
 
+var DEST   = './css/'
+
 gulp.task('default',
   [
-    'build-css'
+    'build_css'
   ], function() {}
 );
 
-gulp.task('build-css', function (cb) {
+var compile_files = {
+  './less/000_compile/_master_compile.less'  : 'style.css',
+  './less/000_compile/_anim_compile.less'    : 'anim.css',
+  './less/000_compile/_utility_compile.less' : 'util.css'
+};
+
+gulp.task('build_css', function () {
   var compile_less = function()
   {
-    var compile_files = {
-      './less/000_compile/_master_compile.less' : 'style.css',
-      './less/000_compile/_anim_compile.less'   : 'anim.css',
-      './less/000_compile/_utility_compile.less': 'util.css'
-    };
 
     for ( var file in compile_files ) {
 
       gulp
         .src(file)
-        .pipe(less({
-          paths: [ path.join(__dirname, 'less', 'includes') ]
-        }))
+        .pipe(less(
+          {
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+          })
+        )
         .pipe(rename(compile_files[file]))
-        .pipe(gulp.dest('./css/'));
-
-      gulp.src('./css/' + compile_files[file])
+        .pipe(gulp.dest(DEST))
         .pipe(clean({compatibility: 'ie8'}))
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest(DEST))
 
     };
-
   };
   return compile_less();
 });
